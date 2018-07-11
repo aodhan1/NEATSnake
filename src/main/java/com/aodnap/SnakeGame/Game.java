73a2976dd5a5280b8f03e2e71b3172874ff29330
Aodhan <aodhann@gmail.com>
2018-07-11 17:05:03 +0100

    Game logic complete

--- src/main/java/com/aodnap/SnakeGame/Game.java
+++ src/main/java/com/aodnap/SnakeGame/Game.java
@@ -28,6 +28,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     private int score;
 
     private Timer timer;
+    private boolean running;
 
     public Game () {
         addKeyListener(this);
@@ -35,15 +36,17 @@ public class Game extends JPanel implements KeyListener, ActionListener {
         setFocusTraversalKeysEnabled(false);
         timer = new Timer(FRAME_DELAY, this);
 
-        imgSnakeHead = new ImageIcon("assets/sprites/head.png");
-        imgSnakeBody = new ImageIcon("assets/sprites/body.png");
-        imgBerry = new ImageIcon("assets/sprites/berry.png");
+        imgSnakeHead = new ImageIcon(getClass().getClassLoader().getResource("assets/sprites/head.png"));
+        imgSnakeBody = new ImageIcon(getClass().getClassLoader().getResource("assets/sprites/body.png"));
+        imgBerry = new ImageIcon(getClass().getClassLoader().getResource("assets/sprites/berry.png"));
 
         initGame();
         timer.start();
+        running = false;
     }
 
     public void paint(Graphics g) {
+        System.out.println("PAINT CALLED");
         //game border
         g.setColor(Color.WHITE);
         g.drawRect(20, 20, 727, 627);
@@ -104,8 +107,10 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        timer.start();
-
+        if(running) {
+            tick();
+            repaint();
+        }
     }
 
     @Override
@@ -115,17 +120,23 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        System.out.print("Key Pressed: ");
+        running = true;
         if(e.getKeyCode() == KeyEvent.VK_RIGHT && snakesDirection != Direction.WEST){
             snakesDirection = Direction.EAST;
+            System.out.println("Right Arrow");
         }
         if(e.getKeyCode() == KeyEvent.VK_LEFT && snakesDirection != Direction.EAST){
             snakesDirection = Direction.WEST;
+            System.out.println("Left Arrow");
         }
         if(e.getKeyCode() == KeyEvent.VK_DOWN && snakesDirection != Direction.NORTH){
             snakesDirection = Direction.SOUTH;
+            System.out.println("Down Arrow");
         }
         if(e.getKeyCode() == KeyEvent.VK_UP && snakesDirection != Direction.SOUTH){
             snakesDirection = Direction.NORTH;
+            System.out.println("Up Arrow");
         }
     }
 
@@ -180,6 +191,56 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     private void plotBerry() {
         grid[berry.getxPos()][berry.getyPos()] = 'B';
-        System.out.println("XPOS: "+berry.getxPos() + " YPOS: "+berry.getyPos());
+        System.out.println("BERRY POSITION XPOS: "+berry.getxPos() + " YPOS: "+berry.getyPos());
+    }
+
+    private void eatBerry(SnakeBodyPart newPart) {
+        score++;
+        placeBerry();
+        growSnake(newPart);
+    }
+
+    private void growSnake(SnakeBodyPart snakeTail) {
+        snake.add(new SnakeBodyPart(snakeTail.getxPos(), snakeTail.getyPos(), 'b', this, snake.getLast()));
+    }
+
+    private void start(){
+        running = true;
+    }
+
+    private void stop() {
+        running = false;
+    }
+
+    private void tick() {
+        //store last position incase snake grows
+        SnakeBodyPart snakeTail = snake.getLast();
+
+        //update each position of the snake
+        ListIterator<SnakeBodyPart> it = snake.listIterator(snake.size());
+        System.out.println("SNAKE LOCATION");
+        while (it.hasPrevious()) {
+            SnakeBodyPart sbp = (SnakeBodyPart) it.previous();
+            sbp.updatePosition();
+            System.out.println("\tPART POSITION XPOS: "+sbp.getxPos() + " YPOS: "+sbp.getyPos());
+        }
+        //check collisions
+        //get snake head position
+        SnakeBodyPart snakeHead = snake.get(0);
+        it = snake.listIterator();
+        while (it.hasNext()) {
+            SnakeBodyPart sbp = (SnakeBodyPart) it.next();
+
+            if (sbp.getType() == 'b' && sbp.getxPos() == snakeHead.getxPos() && sbp.getyPos() == snakeHead.getyPos()) {
+                running = false;
+            }
+        }
+
+        //check collisions with berry
+        if (snakeHead.getxPos() == berry.getxPos() && snakeHead.getyPos() == berry.getyPos()) {
+            eatBerry(snakeTail);
+        }
+
+        plotGrid();
     }
 }
